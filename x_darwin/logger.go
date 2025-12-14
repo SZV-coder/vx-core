85ca03d571c9dfbd31b7027aae9c088d9df844b2
x <5vnetwork@proton.me>
2025-12-14 21:31:55 +0800

    ignore: revert darwin redirect log

--- x_darwin/logger.go
+++ x_darwin/logger.go
@@ -1,3 +1,11 @@
 //go:build darwin
 
 package x_darwin
+
+import (
+	"github.com/5vnetwork/vx-core/common/redirect"
+)
+
+func RedirectStderr(path string) error {
+	return redirect.RedirectStderr(path)
+}
